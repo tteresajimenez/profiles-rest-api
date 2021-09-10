52ee33aa58e5aef7a2287bbcbe9f27e4703a8ce8
Teresa Jimenez <teresa.jimenez@wearemo.com>
2021-09-10 11:32:46 -0500

    Enable Django Admin for user profile model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# Importamos los modelos desde el proyecto profiles API
+from profiles_api import models
 
-# Register your models here.
+"""
+    Aqui registramos los modelos que vamos generando para que el 
+    admin de django los muestre
+
+    Admin de Django -> Permite crear un sitio web de administracion 
+    para inspeccionar la DB y sus modelos
+"""
+# Registramos los modulos
+admin.site.register(models.UserProfile)  # le dice al admin que registre el perfil de usuario
+
