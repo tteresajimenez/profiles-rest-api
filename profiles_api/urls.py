4fb0aaea658c0224a4a5551db6169baad5de817b
Teresa Jimenez <teresa.jimenez@wearemo.com>
2021-09-13 22:38:01 -0500

    Added viewset to profies api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,9 +1,25 @@
-from django.urls import path
+from django.urls import path, include # Este lo agregamos para uso en las viewsets
+# Vamos a importar el default router
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 """
     Lista de URL que concuerden con las vistas del proyecto
 """
+# Para utilizar los default routers los asignamos a una variable
+router = DefaultRouter()
+"""
+    Ahora vamos a definir viewsets especificos en una ruta
+    Genera una lista de urls que estan asociadas al viewset
+    de este modo determina todas las url que son requeridas 
+    para todas las funciones que se definan en el viewset
+"""
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns=[
+    # Registro de la APIView
     path('hello-view/', views.HelloApiView.as_view()), # basicamente de aqui pasa a la peticion get de http
+    # dejamos en blanco al principio porque no queremos poner un prefijo a la url, queremos incluirlas todas
+    path('', include(router.urls)) # despues pasa esta lista de url usando la funcion path y la funcion include
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 # Vamos a usar post o update? si, entonces agregamos esto
 from rest_framework import status  # Para el manejo de respuestas de la api
 from profiles_api import serializers
+# Para utilizar viewsets
+from rest_framework import viewsets
 
 """
     Creamos la clase de la API view esta nos permite definir la
@@ -90,3 +92,62 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+"""
+    Ahora vamos a generar una clase para la viewset estas 
+    se utilizan para mapear las acciones de los objetos 
+    -> list, create, retrieve, update, partial update, destoy
+"""
+
+#  Las funciones que agregamos aqui representan acciones tipicas de objetos
+class HelloViewSet(viewsets.ViewSet):
+    """Test API viewset"""
+    serializer_class = serializers.HelloSerializer # Vamos a utilizar el mismo serializer para la APIViwe y para la viewset
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [   # Lista con las caracteristicas de un viewset
+            'Uses actions (list, create, retrive, update, partial_update',
+            'Automatically maps to URLs using routers',
+            'Provides more functionality with less code',
+        ]
+        # Cuando la peticion se hace al viewset en las URL del proyecto raiz se llamara a la funcion
+        # esta creara una lista (en este caso de caracteristicas de un vier set) y retonarnara
+        # una respuesta en la api
+        return Response({'messaje': 'Hello!',
+                         'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        # A continuacion pasamos la data que se genero en el request y lo pasamos como un atributo en el serializer
+        # NO ENTENDI ESTO BIEN
+        serializer = self.serializer_class(data=request.data)
+
+        # validamos el serializer
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    # Permite devolver un objeto especifico en la viewset
+    # usuarlmente se pasa el ID de la primary key en el request
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its id"""
+        return Response({'http_method': 'GET'}) #Cada funcion concuerda con un metodo http el cual se llama para generar la accion
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http method': 'DELETE'})
