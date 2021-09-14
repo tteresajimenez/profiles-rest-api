4fe48567577c145331d95dde147528497983f226
Teresa Jimenez <teresa.jimenez@wearemo.com>
2021-09-14 15:25:28 -0500

    Added POST PUT PATCH and DELETE to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,9 @@
 """
 from rest_framework.views import APIView
 from rest_framework.response import Response
+# Vamos a usar put o patch? si si entonces
+from rest_framework import status
+from profiles_api import serializers
 
 
 """
@@ -24,7 +27,7 @@ from rest_framework.response import Response
 
 class HelloApiView(APIView):
     """Test API VIEW"""
-
+    serializer_class = serializers.HelloSerializer
     # Aceptamos la peticion HTTP GET
     def get(self, request, format=None): # La variable request viene desde el rest framework y contiene detalles sobre la peticion que se esta haciendo a la api
         """Returns a list of APIView features"""
@@ -37,3 +40,32 @@ class HelloApiView(APIView):
         ]
         # La rta debe contener un diccionario o lista para poder convertir la rta en un json
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
